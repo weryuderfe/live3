d68e9ca5b0136419f8d55e542235383f767b92ad
weryuderfe <67109707+weryuderfe@users.noreply.github.com>
2025-05-13 23:43:03 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -9,3 +9,9 @@ DEFAULT_STREAM_SETTINGS = {
     "bitrate": "3000k",
     "audio_bitrate": "128k"
 }
+
+# Supported video formats
+SUPPORTED_VIDEO_FORMATS = ["mp4", "webm", "mov"]
+
+# Maximum video file size (100MB)
+MAX_VIDEO_SIZE = 100 * 1024 * 1024
