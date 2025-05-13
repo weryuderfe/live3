99cff9f8d9e2325ae38b491941089093f8d57ba5
weryuderfe <67109707+weryuderfe@users.noreply.github.com>
2025-05-13 23:50:02 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,17 +1,8 @@
 # YouTube RTMP streaming configuration
 RTMP_URL = "rtmp://a.rtmp.youtube.com/live2"
 
-# Default stream settings
-DEFAULT_STREAM_SETTINGS = {
-    "stream_preset": "720p30",
-    "resolution": "1280x720",
-    "fps": 30,
-    "bitrate": "3000k",
-    "audio_bitrate": "128k"
-}
-
 # Supported video formats
-SUPPORTED_VIDEO_FORMATS = ["mp4", "webm", "mov"]
+SUPPORTED_VIDEO_FORMATS = ["mp4", "flv"]
 
 # Maximum video file size (100MB)
 MAX_VIDEO_SIZE = 100 * 1024 * 1024
